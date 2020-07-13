b7cbc7b46ca897f18eeb8fb77a3bafb0fd222517
kiranbaakkonen <kiran.baakkonen@gmail.com>
2020-07-13 15:33:26 -0500

    empy

---
+++ wines/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Wine(models.Model):
+    wine_name = models.CharField(max_length=255)
+    price = models.CharField(max_length=10)
+    varietal = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.wine_name
